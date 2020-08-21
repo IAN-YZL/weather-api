e18ee4c97e1e21e85219bd8a961d1849b5ce7c32
Ian Yin <yin.ian093@gmail.com>
2020-08-21 22:34:30 +1000

    restruct the index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const express = require('express');
-
-const app = express();
-const massage = [];
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello');
+const axios = require('axios');
+const instance = axios.create({
+    baseURL: 'api.openweathermap.org/data/2.5',
 });
 
-app.get('/hello', (req, res) => {
-    res.send('Hello from get');
+instance.get('/weather', {
+    params: {
+        q: "Paris",
+        units: 'metric',
+        APPID: "a188b2650dceb0044e221c9ed68ae9b2"
+    }
+}).then(res => {
+    console.log(res);
 });
-
-app.listen(3000, () => {
-
-    console.log('server listening on port 3000');
-});
-  
-
